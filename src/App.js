61f1d32fbf8465b00a447d491c7944d32511d4e2
DoroshFamily <doroshfamily01@gmail.com>
2024-06-03 08:45:35 -0400

    new element

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './sass/componentsass/App.scss';
-import HomePage from './pages/HomePage';
-import LandingPage from './pages/LandingPage';
-import Stay from './pages/Stay';
-import Play from './pages/Play';
-import Dine from './pages/Dine';
-import Shop from './pages/Shop';
-import Events from './pages/Events';
-import Maps from './pages/Maps';
-import { OrientationProvider } from './hooks/OrientationContext';
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './sass/componentsass/App.scss'
+import HomePage from './pages/HomePage'
+import LandingPage from './pages/LandingPage'
+import Stay from './pages/Stay'
+import Play from './pages/Play'
+import Dine from './pages/Dine'
+import Shop from './pages/Shop'
+import Events from './pages/Events'
+import Maps from './pages/Maps'
+import { OrientationProvider } from './hooks/OrientationContext'
 
 function App() {
   useEffect(() => {
     const adjustViewportHeight = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
 
     // Set the viewport height on initial load
-    adjustViewportHeight();
+    adjustViewportHeight()
 
     // Add event listeners
-    window.addEventListener('resize', adjustViewportHeight);
-    window.addEventListener('orientationchange', adjustViewportHeight);
+    window.addEventListener('resize', adjustViewportHeight)
+    window.addEventListener('orientationchange', adjustViewportHeight)
 
     // Cleanup the event listeners on component unmount
     return () => {
-      window.removeEventListener('resize', adjustViewportHeight);
-      window.removeEventListener('orientationchange', adjustViewportHeight);
-    };
-  }, []);
+      window.removeEventListener('resize', adjustViewportHeight)
+      window.removeEventListener('orientationchange', adjustViewportHeight)
+    }
+  }, [])
   return (
     <Router>
-      <OrientationProvider> {/* Providing orientation context */}
-        <div className='mainWrap'>
+      <OrientationProvider>
+        {' '}
+        {/* Providing orientation context */}
+        <div className="mainWrap">
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/home" element={<HomePage />} />
-            <Route path="/stay" element={<Stay pageTitle="Stay"/>} />
-            <Route path="/play" element={<Play pageTitle="Play"/>} />
-            <Route path="/dine" element={<Dine pageTitle="Dine"/>} />
-            <Route path="/shop" element={<Shop pageTitle="Shop"/>} />
-            <Route path="/events" element={<Events pageTitle="Events"/>} />
-            <Route path="/maps" element={<Maps pageTitle="Maps"/>} />
+            <Route path="/stay" element={<Stay pageTitle="Stay" />} />
+            <Route path="/play" element={<Play pageTitle="Play" />} />
+            <Route path="/dine" element={<Dine pageTitle="Dine" />} />
+            <Route path="/shop" element={<Shop pageTitle="Shop" />} />
+            <Route path="/events" element={<Events pageTitle="Events" />} />
+            <Route path="/maps" element={<Maps pageTitle="Maps" />} />
+            <Route path="/maps" element={<Maps pageTitle="Maps" />} />
           </Routes>
         </div>
       </OrientationProvider>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
