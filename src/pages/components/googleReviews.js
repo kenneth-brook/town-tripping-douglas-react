67db513efe709bb33ae6f867347a0e5b1de39661
Kenneth Brook <kenneth.brook@hotmail.com>
2024-07-26 10:09:58 -0400

    logging added

--- src/pages/components/googleReviews.js
+++ src/pages/components/googleReviews.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getGoogleReviews = async (lat, lng, name) => {
   try {
     const url = `/google/getGoogleReviews?lat=${lat}&lng=${lng}&name=${encodeURIComponent(name)}`;
-    console.log(`Fetching Google Reviews for ${name} at (${lat}, ${lng})`);
+    console.log(`Fetching Google Reviews for ${name} at (${lat}, ${lng}) From: ${url}`);
     const response = await axios.get(url);
     console.log('Google Reviews response:', response.data);
     return response.data;
